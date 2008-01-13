2750431f71625be4417d0600f0e9607771170d46
Simon Wunderlich <siwu@hrz.tu-chemnitz.de>
2008-01-13 22:19:38 +0000

    - update author information

--- batman-core/batman-adv-log.h
+++ batman-core/batman-adv-log.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2007-2008 B.A.T.M.A.N. contributors:
- * Marek Lindner
+ * Marek Lindner, Simon Wunderlich
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of version 2 of the GNU General Public
  * License as published by the Free Software Foundation.
