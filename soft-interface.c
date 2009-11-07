47ed27f9706554ce99d93d76f976222ffc328cdc
Simon Wunderlich <siwu@hrz.tu-chemnitz.de>
2009-11-07 10:59:00 +0000

    update README and adjust year numbers in copyright notice

--- soft-interface.c
+++ soft-interface.c
@@ -1,6 +1,8 @@
 /*
- * Copyright (C) 2007-2008 B.A.T.M.A.N. contributors:
+ * Copyright (C) 2007-2009 B.A.T.M.A.N. contributors:
+ *
  * Marek Lindner, Simon Wunderlich
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of version 2 of the GNU General Public
  * License as published by the Free Software Foundation.
