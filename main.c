5d3755ff58f3d091a96372bbb3aba1a0f1045628
Randy Dunlap <randy.dunlap@oracle.com>
2010-07-19 17:23:10 -0700

    batman-adv: don't use default init_module/cleanup_module function names
    Fix two staging drivers to use module_init()/module_exit()
    instead of default init_module() and cleanup_module() function names
    so that there are no name conflicts when both are built-in.
    
    drivers/staging/dt3155/built-in.o: In function `cleanup_module':
    (.text+0xc0): multiple definition of `cleanup_module'
    drivers/staging/batman-adv/built-in.o:(.text+0x330): first defined here
    drivers/staging/dt3155/built-in.o: In function `init_module':
    (.text+0xe60): multiple definition of `init_module'
    drivers/staging/batman-adv/built-in.o:(.text+0x400): first defined here
    
    Signed-off-by: Randy Dunlap <randy.dunlap@oracle.com>
    Signed-off-by: Sven Eckelmann <sven.eckelmann@gmx.de>

--- main.c
+++ main.c
@@ -60,7 +60,7 @@ static struct packet_type batman_adv_packet_type __read_mostly = {
 
 struct workqueue_struct *bat_event_workqueue;
 
-int init_module(void)
+int batman_init(void)
 {
 	int retval;
 
@@ -132,7 +132,7 @@ end:
 	return -ENOMEM;
 }
 
-void cleanup_module(void)
+void batman_exit(void)
 {
 	deactivate_module();
 
@@ -273,6 +273,9 @@ int is_mcast(uint8_t *addr)
 {
 	return *addr & 0x01;
 }
+
+module_init(batman_init);
+module_exit(batman_exit);
 
 MODULE_LICENSE("GPL");
 
