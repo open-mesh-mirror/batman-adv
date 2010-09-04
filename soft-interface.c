3974889a9e77c4310cfa363277e58b8c22833263
Sven Eckelmann <sven.eckelmann@gmx.de>
2010-09-04 22:56:50 +0000

    batman-adv: Don't double free unicast skb on failure
    The soft-interface transmission function expects that the unicast skb is
    still available when a send failed. This is not true on failed search
    for a router. Thus we would try to free the skb twice and create many
    different and hard to debug memory access failures due to access on not
    (anymore) allocated memory.
    
    Reported-by: Andreas Langer <an.langer@gmx.de>
    Signed-off-by: Sven Eckelmann <sven.eckelmann@gmx.de>

--- soft-interface.c
+++ soft-interface.c
@@ -183,7 +183,7 @@ int interface_tx(struct sk_buff *skb, struct net_device *soft_iface)
 	} else {
 		ret = unicast_send_skb(skb, bat_priv);
 		if (ret != 0)
-			goto dropped;
+			goto dropped_freed;
 	}
 
 	bat_priv->stats.tx_packets++;
@@ -191,8 +191,9 @@ int interface_tx(struct sk_buff *skb, struct net_device *soft_iface)
 	goto end;
 
 dropped:
-	bat_priv->stats.tx_dropped++;
 	kfree_skb(skb);
+dropped_freed:
+	bat_priv->stats.tx_dropped++;
 end:
 	return NETDEV_TX_OK;
 }
