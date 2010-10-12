af22dfdd8e4266467c1998d9ea65f93a82aa7e18
Linus Lüssing <linus.luessing@web.de>
2010-10-12 09:56:38 +0000

    batman-adv: Fix wrongly formatted %pM in bat_dbg()
    bat_dbg() invokes debug_log() which invokes the kernel's vscnprintf(),
    which invokes the kernel's vsnprintf(), and not the customized
    bat_vsnprintf(), which backports the %pM to older kernel versions!
    
    Therefore this commit ports a customized vscnprintf() to bat_printk.c,
    too, making mac addresses being displayed correctly with bat_dbg()
    again.
    
    Signed-off-by: Linus Lüssing <linus.luessing@web.de>
    Signed-off-by: Marek Lindner <lindner_marek@yahoo.de>

--- bat_printk.c
+++ bat_printk.c
@@ -838,6 +838,31 @@ static int bat_vsnprintf(char *buf, size_t size, const char *fmt, va_list args)
 }
 
 /**
+ * bat_vscnprintf - Format a string and place it in a buffer
+ * @buf: The buffer to place the result into
+ * @size: The size of the buffer, including the trailing null space
+ * @fmt: The format string to use
+ * @args: Arguments for the format string
+ *
+ * The return value is the number of characters which have been written into
+ * the @buf not including the trailing '\0'. If @size is <= 0 the function
+ * returns 0.
+ *
+ * Call this function if you are already dealing with a va_list.
+ * You probably want scnprintf() instead.
+ *
+ * See the vsnprintf() documentation for format string extensions over C99.
+ */
+int bat_vscnprintf(char *buf, size_t size, const char *fmt, va_list args)
+{
+        int i;
+
+        i = bat_vsnprintf(buf, size, fmt, args);
+
+        return (i >= size) ? (size - 1) : i;
+}
+
+/**
  * bat_printk - print a kernel message using extra %p formatting
  * strings, forward compatible with kernel version 2.6.31 printk, minus
  * *p[sS].
