b59a44eeb7c179f5f401fe71e9c270075b15864a
Sven Eckelmann <sven.eckelmann@gmx.de>
2010-10-25 21:49:18 +0000

    batman-adv: Cleanup compat.h
    Following functions/defines were declared more than once:
     * skb_mac_header
    
    Following functions/defines weren't used anymore:
     * skb_clone_writable
     * dev_get_stats
    
    Signed-off-by: Sven Eckelmann <sven.eckelmann@gmx.de>

--- compat.h
+++ compat.h
@@ -42,9 +42,6 @@
 #define skb_mac_header(_skb) \
     ((_skb)->mac.raw)
 
-#define skb_mac_header(_skb) \
-    ((_skb)->mac.raw)
-
 #include <linux/etherdevice.h>
 static inline __be16 bat_eth_type_trans(struct sk_buff *skb,
 					struct net_device *dev)
@@ -59,12 +56,6 @@ static inline __be16 bat_eth_type_trans(struct sk_buff *skb,
 #endif /* < KERNEL_VERSION(2,6,22) */
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 23)
-
-static inline int skb_clone_writable(struct sk_buff *skb, unsigned int len)
-{
-	/* skb->hdr_len not available, just "not writable" to enforce a copy */
-	return 0;
-}
 
 static inline int skb_cow_head(struct sk_buff *skb, unsigned int headroom)
 {
@@ -266,14 +257,6 @@ int bat_snprintf(char *buf, size_t size, const char *fmt, ...);
 
 int bat_seq_printf(struct seq_file *m, const char *f, ...);
 #define seq_printf bat_seq_printf
-
-static inline struct net_device_stats *dev_get_stats(struct net_device *dev)
-{
-	if (dev->get_stats)
-		return dev->get_stats(dev);
-	else
-		return NULL;
-}
 
 #endif /* < KERNEL_VERSION(2, 6, 29) */
 
