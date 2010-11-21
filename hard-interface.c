5d4b5a4d11afe5ce05e3e5aed0897b45b7cfc444
Sven Eckelmann <sven.eckelmann@gmx.de>
2010-11-21 20:02:49 +0000

    batman-adv: Don't remove interface with spinlock held
    We call a lot of the netdevice code when holding if_list_lock which will
    spin the whole time. This is not necessary because we only want to
    protect the access to the list to be serialized. An extra queue can be
    used which hold all interfaces which should be removed and then use that
    queue without any locks for netdevice cleanup.
    
    Signed-off-by: Sven Eckelmann <sven.eckelmann@gmx.de>

--- hard-interface.c
+++ hard-interface.c
@@ -469,9 +469,6 @@ static void hardif_remove_interface(struct batman_if *batman_if)
 		return;
 
 	batman_if->if_status = IF_TO_BE_REMOVED;
-
-	/* caller must take if_list_lock */
-	list_del_rcu(&batman_if->list);
 	sysfs_del_hardif(&batman_if->hardif_obj);
 	call_rcu(&batman_if->rcu, hardif_free_rcu);
 }
@@ -479,13 +476,21 @@ static void hardif_remove_interface(struct batman_if *batman_if)
 void hardif_remove_interfaces(void)
 {
 	struct batman_if *batman_if, *batman_if_tmp;
+	struct list_head if_list_queue;
 
-	rtnl_lock();
+	INIT_LIST_HEAD(&if_list_queue);
+
 	spin_lock(&if_list_lock);
 	list_for_each_entry_safe(batman_if, batman_if_tmp, &if_list, list) {
-		hardif_remove_interface(batman_if);
+		list_del_rcu(&batman_if->list);
+		list_add_tail(&batman_if->list, &if_list_queue);
 	}
 	spin_unlock(&if_list_lock);
+
+	rtnl_lock();
+	list_for_each_entry_safe(batman_if, batman_if_tmp, &if_list_queue, list) {
+		hardif_remove_interface(batman_if);
+	}
 	rtnl_unlock();
 }
 
@@ -512,8 +517,10 @@ static int hard_if_event(struct notifier_block *this,
 		break;
 	case NETDEV_UNREGISTER:
 		spin_lock(&if_list_lock);
-		hardif_remove_interface(batman_if);
+		list_del_rcu(&batman_if->list);
 		spin_unlock(&if_list_lock);
+
+		hardif_remove_interface(batman_if);
 		break;
 	case NETDEV_CHANGEMTU:
 		if (batman_if->soft_iface)
