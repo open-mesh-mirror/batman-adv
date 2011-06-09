open-mesh-mirror/batman-adv

commit 4d30670880c84071dba4b220f595e64d6c01d1ba

batman-adv: accept delayed rebroadcasts to avoid bogus routing under heavy load
When a link is saturated (re)broadcasts of OGMs are delayed. Under heavy load
this delay may exceed the orig interval which leads to OGMs being dropped (the
code would only accept an OGM rebroadcast if it arrived before the next OGM was
broadcasted). With this patch batman-adv will also accept delayed OGMs in order
to avoid a bogus influence on the routing metric.

Signed-off-by: Daniele Furlan <daniele.furlan@gmail.com>

routing.c
@@ -720,17 +720,15 @@ void receive_bat_packet(const struct ethhdr *ethhdr,
 
 		/* neighbor has to indicate direct link and it has to
 		 * come via the corresponding interface */
-		/* if received seqno equals last send seqno save new
-		 * seqno for bidirectional check */
+		/* save packet seqno for bidirectional check */
 		if (has_directlink_flag &&
 		    compare_eth(if_incoming->net_dev->dev_addr,
-				batman_packet->orig) &&
-		    (batman_packet->seqno - if_incoming_seqno + 2 == 0)) {
+				batman_packet->orig)) {
 			offset = if_incoming->if_num * NUM_WORDS;
 
 			spin_lock_bh(&orig_neigh_node->ogm_cnt_lock);
 			word = &(orig_neigh_node->bcast_own[offset]);
-			bit_mark(word, 0);
+			bit_mark(word, if_incoming_seqno - batman_packet->seqno - 2);
 			orig_neigh_node->bcast_own_sum[if_incoming->if_num] =
 				bit_packet_count(word);
 			spin_unlock_bh(&orig_neigh_node->ogm_cnt_lock);

commit da04ca5687925cba81d9783a76633b13379db0f9

batman-adv: Fix line over 80 characters
Regression was introduced in 4d30670880c84071dba4b220f595e64d6c01d1ba.

Signed-off-by: Sven Eckelmann <sven@narfation.org>

routing.c
@@ -728,7 +728,8 @@ void receive_bat_packet(const struct ethhdr *ethhdr,
 
 			spin_lock_bh(&orig_neigh_node->ogm_cnt_lock);
 			word = &(orig_neigh_node->bcast_own[offset]);
-			bit_mark(word, if_incoming_seqno - batman_packet->seqno - 2);
+			bit_mark(word,
+				 if_incoming_seqno - batman_packet->seqno - 2);
 			orig_neigh_node->bcast_own_sum[if_incoming->if_num] =
 				bit_packet_count(word);
 			spin_unlock_bh(&orig_neigh_node->ogm_cnt_lock);