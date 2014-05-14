236e60395e67b8311185cf932a899898773c6210
Antonio Quartulli <antonio@meshcoding.com>
2014-05-14 14:23:15 +0200

    batman-adv: fix typ0 in kerneldoc and comment
    Introduced by 9b9cdbe28e2b9c8bdf9c761f22ba9655963d13d4
    ("batman-adv: fix removing neigh_ifinfo")
    
    Signed-off-by: Antonio Quartulli <antonio@meshcoding.com>
    Reported-by: Sergei Shtylyov <sergei.shtylyov@cogentembedded.com>
    Signed-off-by: Marek Lindner <mareklindner@neomailbox.ch>

--- originator.c
+++ originator.c
@@ -704,7 +704,7 @@ free_orig_node:
 /**
  * batadv_purge_neigh_ifinfo - purge obsolete ifinfo entries from neighbor
  * @bat_priv: the bat priv with all the soft interface information
- * @neigh_node: orig node which is to be checked
+ * @neigh: orig node which is to be checked
  */
 static void
 batadv_purge_neigh_ifinfo(struct batadv_priv *bat_priv,
@@ -842,7 +842,7 @@ batadv_purge_orig_neighbors(struct batadv_priv *bat_priv,
 			hlist_del_rcu(&neigh_node->list);
 			batadv_neigh_node_free_ref(neigh_node);
 		} else {
-			/* only neccesary if not the whole neighbor is to be
+			/* only necessary if not the whole neighbor is to be
 			 * deleted, but some interface has been removed.
 			 */
 			batadv_purge_neigh_ifinfo(bat_priv, neigh_node);
