c7203da7a3aeec3358e60fe970f2aa654e5ca69c
Joe Perches <joe@perches.com>
2015-02-16 17:31:39 -0800

    batman-adv: Remove uses of return value of seq_printf
    This function is soon going to return void so remove the
    return value use.
    
    Convert the return value to test seq_has_overflowed() instead.
    
    Signed-off-by: Joe Perches <joe@perches.com>
    Signed-off-by: Marek Lindner <mareklindner@neomailbox.ch>

--- compat.h
+++ compat.h
@@ -464,4 +464,13 @@ static int __batadv_interface_kill_vid(struct net_device *dev, __be16 proto,\
 
 #endif /* < KERNEL_VERSION(3, 17, 0) */
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3, 19, 0)
+
+static inline bool seq_has_overflowed(struct seq_file *m)
+{
+	return m->count == m->size;
+}
+
+#endif /* < KERNEL_VERSION(3, 19, 0) */
+
 #endif /* _NET_BATMAN_ADV_COMPAT_H_ */
