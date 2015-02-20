open-mesh-mirror/batman-adv

commit fe67bb41f0e1647c3b05aeb4b736a1171bff496b

batman-adv: debugfs, avoid compiling for !DEBUG_FS
Normally the debugfs framework will return error pointer with -ENODEV
for function calls when DEBUG_FS is not set.

batman does not notice this error code and continues trying to create
debugfs files and executes more code. We can avoid this code execution
by disabling compiling debugfs.c when DEBUG_FS is not set.

Signed-off-by: Markus Pargmann <mpa@pengutronix.de>
Signed-off-by: Marek Lindner <mareklindner@neomailbox.ch>

debugfs.h
@@ -20,11 +20,45 @@
 
 #define BATADV_DEBUGFS_SUBDIR "batman_adv"
 
+#if IS_ENABLED(CONFIG_DEBUG_FS)
+
 void batadv_debugfs_init(void);
 void batadv_debugfs_destroy(void);
 int batadv_debugfs_add_meshif(struct net_device *dev);
 void batadv_debugfs_del_meshif(struct net_device *dev);
 int batadv_debugfs_add_hardif(struct batadv_hard_iface *hard_iface);
 void batadv_debugfs_del_hardif(struct batadv_hard_iface *hard_iface);
+
+#else
+
+static inline void batadv_debugfs_init(void)
+{
+}
+
+static inline void batadv_debugfs_destroy(void)
+{
+}
+
+static inline int batadv_debugfs_add_meshif(struct net_device *dev)
+{
+	return 0;
+}
+
+static inline void batadv_debugfs_del_meshif(struct net_device *dev)
+{
+}
+
+static inline int batadv_debugfs_add_hardif(
+		struct batadv_hard_iface *hard_iface)
+{
+	return 0;
+}
+
+static inline void batadv_debugfs_del_hardif(
+		struct batadv_hard_iface *hard_iface)
+{
+}
+
+#endif
 
 #endif /* _NET_BATMAN_ADV_DEBUGFS_H_ */

Makefile.kbuild
@@ -20,7 +20,7 @@ obj-$(CONFIG_BATMAN_ADV) += batman-adv.o
 batman-adv-y += bat_iv_ogm.o
 batman-adv-y += bitarray.o
 batman-adv-$(CONFIG_BATMAN_ADV_BLA) += bridge_loop_avoidance.o
-batman-adv-y += debugfs.o
+batman-adv-$(CONFIG_DEBUG_FS) += debugfs.o
 batman-adv-$(CONFIG_BATMAN_ADV_DAT) += distributed-arp-table.o
 batman-adv-y += fragmentation.o
 batman-adv-y += gateway_client.o

debugfs.c
@@ -482,11 +482,7 @@ rem_attr:
 	debugfs_remove_recursive(hard_iface->debug_dir);
 	hard_iface->debug_dir = NULL;
 out:
-#ifdef CONFIG_DEBUG_FS
 	return -ENOMEM;
-#else
-	return 0;
-#endif /* CONFIG_DEBUG_FS */
 }
 
 /**
@@ -541,11 +537,7 @@ rem_attr:
 	debugfs_remove_recursive(bat_priv->debug_dir);
 	bat_priv->debug_dir = NULL;
 out:
-#ifdef CONFIG_DEBUG_FS
 	return -ENOMEM;
-#else
-	return 0;
-#endif /* CONFIG_DEBUG_FS */
 }
 
 void batadv_debugfs_del_meshif(struct net_device *dev)

commit 2f9d8a204227ab1f485852cc52f8bdb3ccb4ee18

batman-adv: checkpatch - wrong alignment
Introduced by fe67bb41f0e1647c3b05aeb4b736a1171bff496b
("batman-adv: debugfs, avoid compiling for !DEBUG_FS")

Signed-off-by: Marek Lindner <mareklindner@neomailbox.ch>

debugfs.h
@@ -48,14 +48,14 @@ static inline void batadv_debugfs_del_meshif(struct net_device *dev)
 {
 }
 
-static inline int batadv_debugfs_add_hardif(
-		struct batadv_hard_iface *hard_iface)
+static inline
+int batadv_debugfs_add_hardif(struct batadv_hard_iface *hard_iface)
 {
 	return 0;
 }
 
-static inline void batadv_debugfs_del_hardif(
-		struct batadv_hard_iface *hard_iface)
+static inline
+void batadv_debugfs_del_hardif(struct batadv_hard_iface *hard_iface)
 {
 }
 