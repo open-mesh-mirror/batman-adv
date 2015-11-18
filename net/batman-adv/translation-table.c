open-mesh-mirror/batman-adv

commit af5fa950cbb6b6c4121763ea8d505a30219a3e2e

batman-adv: fix erroneous client entry duplicate detection
The translation table implementation, namely batadv_compare_tt(),
is used to compare two client entries and deciding if they are the
holding the same information. Each client entry is identified by
its mac address and its VLAN id (VID).
Consequently, batadv_compare_tt() has to not only compare the mac
addresses but also the VIDs.

Without this fix adding a new client entry that possesses the same
mac address as another client but operates on a different VID will
fail because both client entries will considered identical.

Signed-off-by: Marek Lindner <mareklindner@neomailbox.ch>

net/batman-adv/translation-table.c
@@ -67,13 +67,15 @@ static void batadv_tt_global_del(struct batadv_priv *bat_priv,
 				 unsigned short vid, const char *message,
 				 bool roaming);
 
-/* returns 1 if they are the same mac addr */
+/* returns 1 if they are the same mac addr and vid */
 static int batadv_compare_tt(const struct hlist_node *node, const void *data2)
 {
 	const void *data1 = container_of(node, struct batadv_tt_common_entry,
 					 hash_entry);
+	const struct batadv_tt_common_entry* tt1 = data1;
+	const struct batadv_tt_common_entry* tt2 = data2;
 
-	return batadv_compare_eth(data1, data2);
+	return (tt1->vid == tt2->vid) && batadv_compare_eth(data1, data2);
 }
 
 /**

commit 5019fc0faac0d8f0b377dbc7d27a3899c45237c3

batman-adv: Fix variable declaration warning in batadv_compare_tt
checkpatch is warning about the wrong way a pointer is declared in
batadv_compare_tt.

    ERROR: "foo* bar" should be "foo *bar"
    #80: FILE: ./net/batman-adv/translation-table.c:80:
    +	const struct batadv_tt_common_entry* tt1 = data1;

Fixes: af5fa950cbb6 ("batman-adv: fix erroneous client entry duplicate detection")
Signed-off-by: Sven Eckelmann <sven@narfation.org>
Signed-off-by: Marek Lindner <mareklindner@neomailbox.ch>

net/batman-adv/translation-table.c
@@ -72,8 +72,8 @@ static int batadv_compare_tt(const struct hlist_node *node, const void *data2)
 {
 	const void *data1 = container_of(node, struct batadv_tt_common_entry,
 					 hash_entry);
-	const struct batadv_tt_common_entry* tt1 = data1;
-	const struct batadv_tt_common_entry* tt2 = data2;
+	const struct batadv_tt_common_entry *tt1 = data1;
+	const struct batadv_tt_common_entry *tt2 = data2;
 
 	return (tt1->vid == tt2->vid) && batadv_compare_eth(data1, data2);
 }