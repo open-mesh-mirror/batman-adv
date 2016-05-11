open-mesh-mirror/batman-adv

commit 71e957cd30a0b17182c5247935b6d4c127f12265

batman-adv: init ELP tweaking options only once
The ELP interval and throughput override interface settings are initialized
with default settings on every time an interface is added to a mesh.
This patch prevents this behavior by moving the configuration init to the
interface detection routine which runs only once per interface.

Signed-off-by: Marek Lindner <mareklindner@neomailbox.ch>

net/batman-adv/hard-interface.c
@@ -683,6 +683,13 @@ batadv_hardif_add_interface(struct net_device *net_dev)
 	if (batadv_is_wifi_netdev(net_dev))
 		hard_iface->num_bcasts = BATADV_NUM_BCASTS_WIRELESS;
 
+	/* enable link throughput auto-detection by setting the throughput
+	 * override to zero
+	 */
+	atomic_set(&hard_iface->bat_v.throughput_override, 0);
+
+	atomic_set(&hard_iface->bat_v.elp_interval, 500);
+
 	/* extra reference for return */
 	kref_init(&hard_iface->refcount);
 	kref_get(&hard_iface->refcount);

net/batman-adv/bat_v.c
@@ -70,11 +70,6 @@ static int batadv_v_iface_enable(struct batadv_hard_iface *hard_iface)
 	if (ret < 0)
 		batadv_v_elp_iface_disable(hard_iface);
 
-	/* enable link throughput auto-detection by setting the throughput
-	 * override to zero
-	 */
-	atomic_set(&hard_iface->bat_v.throughput_override, 0);
-
 	return ret;
 }
 

net/batman-adv/bat_v_elp.c
@@ -344,7 +344,6 @@ int batadv_v_elp_iface_enable(struct batadv_hard_iface *hard_iface)
 	/* randomize initial seqno to avoid collision */
 	get_random_bytes(&random_seqno, sizeof(random_seqno));
 	atomic_set(&hard_iface->bat_v.elp_seqno, random_seqno);
-	atomic_set(&hard_iface->bat_v.elp_interval, 500);
 
 	/* assume full-duplex by default */
 	hard_iface->bat_v.flags |= BATADV_FULL_DUPLEX;

commit ffd2f27908e5bb532aadcc6bcc5a69dbeba758dd

batman-adv: Only init ELP tweaking options when BATMAN_V is enabled
The build currently fails with CONFIG_BATMAN_ADV_BATMAN_V=n because
hard_iface->bat_v is only defined when CONFIG_BATMAN_ADV_BATMAN_V=y is set.
The initialization of bat_v.throughput_override/bat_v.elp_interval
must therefore only be compiled in when also BATMAN_V is enabled.

Fixes: 71e957cd30a0 ("batman-adv: init ELP tweaking options only once")
Signed-off-by: Sven Eckelmann <sven.eckelmann@open-mesh.com>
Signed-off-by: Marek Lindner <mareklindner@neomailbox.ch>

net/batman-adv/hard-interface.c
@@ -683,12 +683,14 @@ batadv_hardif_add_interface(struct net_device *net_dev)
 	if (batadv_is_wifi_netdev(net_dev))
 		hard_iface->num_bcasts = BATADV_NUM_BCASTS_WIRELESS;
 
+#ifdef CONFIG_BATMAN_ADV_BATMAN_V
 	/* enable link throughput auto-detection by setting the throughput
 	 * override to zero
 	 */
 	atomic_set(&hard_iface->bat_v.throughput_override, 0);
 
 	atomic_set(&hard_iface->bat_v.elp_interval, 500);
+#endif
 
 	/* extra reference for return */
 	kref_init(&hard_iface->refcount);