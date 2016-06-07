open-mesh-mirror/batman-adv

commit ffd2f27908e5bb532aadcc6bcc5a69dbeba758dd

batman-adv: Only init ELP tweaking options when BATMAN_V is enabled
The build currently fails with CONFIG_BATMAN_ADV_BATMAN_V=n because
hard_iface->bat_v is only defined when CONFIG_BATMAN_ADV_BATMAN_V=y is set.
The initialization of bat_v.throughput_override/bat_v.elp_interval
must therefore only be compiled in when also BATMAN_V is enabled.

Fixes: 71e957cd30a0 ("batman-adv: init ELP tweaking options only once")
Signed-off-by: Sven Eckelmann <sven.eckelmann@open-mesh.com>
Signed-off-by: Marek Lindner <mareklindner@neomailbox.ch>

net/batman-adv/hard-interface.c
@@ -683,12 +683,14 @@ batadv_hardif_add_interface(struct net_device *net_dev)
 	if (batadv_is_wifi_netdev(net_dev))
 		hard_iface->num_bcasts = BATADV_NUM_BCASTS_WIRELESS;
 
+#ifdef CONFIG_BATMAN_ADV_BATMAN_V
 	/* enable link throughput auto-detection by setting the throughput
 	 * override to zero
 	 */
 	atomic_set(&hard_iface->bat_v.throughput_override, 0);
 
 	atomic_set(&hard_iface->bat_v.elp_interval, 500);
+#endif
 
 	/* extra reference for return */
 	kref_init(&hard_iface->refcount);

commit 713586fd3e270b00272db6be94dcf7e4a414af1d

batman-adv: B.A.T.M.A.N. V - implement hard-iface init function
Some fields in the hard-interface data structure are specific to the
B.A.T.M.A.N. V protocol and have to be initialized only when such
protocol is compiled in.
Instead of having a #ifdef block in the middle of the hard-interface.c
code it is better to have an algorithm private function that hides the
precompiler logic in its own header file (like other functions).

Fixes: ffd2f27908e5 ("batman-adv: Only init ELP tweaking options when BATMAN_V is enabled")
Signed-off-by: Antonio Quartulli <a@unstable.cc>
Reviewed-by: Sven Eckelmann <sven@narfation.org>
Signed-off-by: Marek Lindner <mareklindner@neomailbox.ch>

net/batman-adv/bat_v.c
@@ -18,6 +18,7 @@
 #include "bat_v.h"
 #include "main.h"
 
+#include <linux/atomic.h>
 #include <linux/bug.h>
 #include <linux/cache.h>
 #include <linux/init.h>
@@ -332,6 +333,20 @@ static struct batadv_algo_ops batadv_batman_v __read_mostly = {
 	.bat_neigh_is_similar_or_better = batadv_v_neigh_is_sob,
 	.bat_neigh_print = batadv_v_neigh_print,
 };
+
+/**
+ * batadv_v_hardif_init - initialize the algorithm specific fields in the
+ *  hard-interface object
+ * @hard_iface: the hard-interface to initialize
+ */
+void batadv_v_hardif_init(struct batadv_hard_iface *hard_iface)
+{
+	/* enable link throughput auto-detection by setting the throughput
+	 * override to zero
+	 */
+	atomic_set(&hard_iface->bat_v.throughput_override, 0);
+	atomic_set(&hard_iface->bat_v.elp_interval, 500);
+}
 
 /**
  * batadv_v_mesh_init - initialize the B.A.T.M.A.N. V private resources for a

net/batman-adv/bat_v.h
@@ -23,6 +23,7 @@
 #ifdef CONFIG_BATMAN_ADV_BATMAN_V
 
 int batadv_v_init(void);
+void batadv_v_hardif_init(struct batadv_hard_iface *hardif);
 int batadv_v_mesh_init(struct batadv_priv *bat_priv);
 void batadv_v_mesh_free(struct batadv_priv *bat_priv);
 
@@ -31,6 +32,10 @@ void batadv_v_mesh_free(struct batadv_priv *bat_priv);
 static inline int batadv_v_init(void)
 {
 	return 0;
+}
+
+static inline void batadv_v_hardif_init(struct batadv_hard_iface *hardif)
+{
 }
 
 static inline int batadv_v_mesh_init(struct batadv_priv *bat_priv)

net/batman-adv/hard-interface.c
@@ -37,6 +37,7 @@
 #include <linux/spinlock.h>
 #include <linux/workqueue.h>
 
+#include "bat_v.h"
 #include "bridge_loop_avoidance.h"
 #include "debugfs.h"
 #include "distributed-arp-table.h"
@@ -684,14 +685,7 @@ batadv_hardif_add_interface(struct net_device *net_dev)
 	if (batadv_is_wifi_netdev(net_dev))
 		hard_iface->num_bcasts = BATADV_NUM_BCASTS_WIRELESS;
 
-#ifdef CONFIG_BATMAN_ADV_BATMAN_V
-	/* enable link throughput auto-detection by setting the throughput
-	 * override to zero
-	 */
-	atomic_set(&hard_iface->bat_v.throughput_override, 0);
-
-	atomic_set(&hard_iface->bat_v.elp_interval, 500);
-#endif
+	batadv_v_hardif_init(hard_iface);
 
 	/* extra reference for return */
 	kref_init(&hard_iface->refcount);