2f9f52d1dfc417201028cc65f91aaddc5eaf493a
Sven Eckelmann <sven@narfation.org>
2016-10-18 15:57:39 +0200

    batman-adv: compat: Move netlink const compat to coccinelle
    Signed-off-by: Sven Eckelmann <sven@narfation.org>
    Signed-off-by: Simon Wunderlich <sw@simonwunderlich.de>

---
+++ compat-patches/0002-genl-const.cocci
@@ -0,0 +1,10 @@
+@@
+attribute __genl_const;
+@@
+(
+-const struct genl_multicast_group
++__genl_const struct genl_multicast_group
+|
+-const struct genl_ops
++__genl_const struct genl_ops
+)
--- compat-patches/replacements.sh
+++ compat-patches/replacements.sh
@@ -1,9 +1,3 @@
 #! /bin/sh
 
 set -e
-
-# for kernel < 3.13 to make netlink compat code work
-sed -i \
-	-e 's/^static const struct genl_multicast_group batadv_netlink_mcgrps/static __genl_const struct genl_multicast_group batadv_netlink_mcgrps/' \
-	-e 's/^static const struct genl_ops batadv_netlink_ops/static __genl_const struct genl_ops batadv_netlink_ops/' \
-	build/net/batman-adv/netlink.c
