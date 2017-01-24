3da7de5d6aa8f69d6d7daa62f3e6d3f7ebed5772
Linus Lüssing <linus.luessing@c0d3.blue>
2017-01-24 01:00:30 +0100

    batman-adv: restructure cfg80211 compat code
    This commit restructures the compat code regarding cfg80211 to achieve
    the following:
    
    a) Allows to compile kernels < 3.16 with -Werror by replacing the
    "#warning" with a runtime warning through pr_warn_once().
    b) Allows to run < 3.16 kernels without warnings if no cfg80211 based
    interface is used.
    c) Migrates cfg80211 compat code from compat.h to compat cfg80211.h to
    keep compat.h slim.
    
    Signed-off-by: Linus Lüssing <linus.luessing@c0d3.blue>
    Signed-off-by: Sven Eckelmann <sven@narfation.org>

--- compat-include/net/cfg80211.h
+++ compat-include/net/cfg80211.h
@@ -4,9 +4,28 @@
 #include <linux/version.h>
 #include_next <net/cfg80211.h>
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3, 16, 0)
+
+static inline int cfg80211_get_station(struct net_device *dev,
+				       const u8 *mac_addr,
+				       struct station_info *sinfo)
+{
+	pr_warn_once("cfg80211 based throughput metric is only supported with Linux 3.16+\n");
+	return -ENOENT;
+}
+
+/* The following define substitutes the expected_throughput field with a random
+ * one existing in the station_info struct. It can be random because due to the
+ * function above it will never be used. Only needed to make the code compile
+ */
+#define expected_throughput filled
+
+#endif /* < KERNEL_VERSION(3, 16, 0) */
+
 #if LINUX_VERSION_CODE < KERNEL_VERSION(4, 8, 0)
 
-#if !IS_ENABLED(CONFIG_CFG80211)
+#if !IS_ENABLED(CONFIG_CFG80211) && \
+    LINUX_VERSION_CODE >= KERNEL_VERSION(3, 16, 0)
 
 #define cfg80211_get_station(dev, mac_addr, sinfo) \
 	batadv_cfg80211_get_station(dev, mac_addr, sinfo)
