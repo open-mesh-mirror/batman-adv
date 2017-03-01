open-mesh-mirror/batman-adv

commit b4008f675b3737277728a22699166fcb15007c5a

batman-adv: Remove preprocessor checks in multicast.c
It is not necessary to disable these code sections in case other kernel
features are disabled. Instead the IS_ENABLED tests can be added directly
in the code and the compiler can remove the unnecessary code parts during
its optimization run.

Signed-off-by: Sven Eckelmann <sven@narfation.org>

net/batman-adv/multicast.c
@@ -182,10 +182,8 @@ static void batadv_mcast_mla_br_addr_cpy(char *dst, const struct br_ip *src)
 {
 	if (src->proto == htons(ETH_P_IP))
 		ip_eth_mc_map(src->u.ip4, dst);
-#if IS_ENABLED(CONFIG_IPV6)
-	else if (src->proto == htons(ETH_P_IPV6))
+	else if (IS_ENABLED(CONFIG_IPV6) && src->proto == htons(ETH_P_IPV6))
 		ipv6_eth_mc_map(&src->u.ip6, dst);
-#endif
 	else
 		eth_zero_addr(dst);
 }
@@ -494,9 +492,8 @@ static bool batadv_mcast_mla_tvlv_update(struct batadv_priv *bat_priv)
 	if (!bridged)
 		goto update;
 
-#if !IS_ENABLED(CONFIG_BRIDGE_IGMP_SNOOPING)
-	pr_warn_once("No bridge IGMP snooping compiled - multicast optimizations disabled\n");
-#endif
+	if (!IS_ENABLED(CONFIG_BRIDGE_IGMP_SNOOPING))
+		pr_warn_once("No bridge IGMP snooping compiled - multicast optimizations disabled\n");
 
 	querier4.exists = br_multicast_has_querier_anywhere(dev, ETH_P_IP);
 	querier4.shadowing = br_multicast_has_querier_adjacent(dev, ETH_P_IP);
@@ -671,7 +668,6 @@ static int batadv_mcast_forw_mode_check_ipv4(struct batadv_priv *bat_priv,
 	return 0;
 }
 
-#if IS_ENABLED(CONFIG_IPV6)
 /**
  * batadv_mcast_is_report_ipv6 - check for MLD reports
  * @skb: the ethernet frame destined for the mesh
@@ -736,7 +732,6 @@ static int batadv_mcast_forw_mode_check_ipv6(struct batadv_priv *bat_priv,
 
 	return 0;
 }
-#endif
 
 /**
  * batadv_mcast_forw_mode_check - check for optimized forwarding potential
@@ -765,11 +760,12 @@ static int batadv_mcast_forw_mode_check(struct batadv_priv *bat_priv,
 	case ETH_P_IP:
 		return batadv_mcast_forw_mode_check_ipv4(bat_priv, skb,
 							 is_unsnoopable);
-#if IS_ENABLED(CONFIG_IPV6)
 	case ETH_P_IPV6:
+		if (!IS_ENABLED(CONFIG_IPV6))
+			return -EINVAL;
+
 		return batadv_mcast_forw_mode_check_ipv6(bat_priv, skb,
 							 is_unsnoopable);
-#endif
 	default:
 		return -EINVAL;
 	}

commit 528346332e739e0d36599d42638f8f8f848de378

batman-adv: Re-add preprocessor check in batadv_mcast_mla_br_addr_cpy
The union member ip6 in br_ip::u is not available when the kernel is build
without IPv6 support. The code accessing this member must therefore be
protected by a preprocessor check which tests for CONFIG_IPV6.

Reported-by: kbuild test robot <fengguang.wu@intel.com>
Fixes: b4008f675b37 ("batman-adv: Remove preprocessor checks in multicast.c")
Signed-off-by: Sven Eckelmann <sven@narfation.org>

net/batman-adv/multicast.c
@@ -182,8 +182,10 @@ static void batadv_mcast_mla_br_addr_cpy(char *dst, const struct br_ip *src)
 {
 	if (src->proto == htons(ETH_P_IP))
 		ip_eth_mc_map(src->u.ip4, dst);
-	else if (IS_ENABLED(CONFIG_IPV6) && src->proto == htons(ETH_P_IPV6))
+#if IS_ENABLED(CONFIG_IPV6)
+	else if (src->proto == htons(ETH_P_IPV6))
 		ipv6_eth_mc_map(&src->u.ip6, dst);
+#endif
 	else
 		eth_zero_addr(dst);
 }