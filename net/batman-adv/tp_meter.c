open-mesh-mirror/batman-adv

commit 412f93cbbfa81e31773a513604eb3c7ff5ec814d

batman-adv: tp_meter: mark init function with __init
batadv_tp_meter_init() is invoked in batadv_init() only
which is marked with __init.

For this reason batadv_tp_meter_init() can be marked with
__init as well and dropped after module load.

Signed-off-by: Antonio Quartulli <a@unstable.cc>
Signed-off-by: Sven Eckelmann <sven@narfation.org>

net/batman-adv/tp_meter.c
@@ -1497,7 +1497,7 @@ void batadv_tp_meter_recv(struct batadv_priv *bat_priv, struct sk_buff *skb)
 /**
  * batadv_tp_meter_init - initialize global tp_meter structures
  */
-void batadv_tp_meter_init(void)
+void __init batadv_tp_meter_init(void)
 {
 	get_random_bytes(batadv_tp_prerandom, sizeof(batadv_tp_prerandom));
 }

commit 5f9fd02a94cb004d4886303969ba10cc169aefef

batman-adv: tp_meter: Add missing header for __init
Fixes: 412f93cbbfa8 ("batman-adv: tp_meter: mark init function with __init")
Signed-off-by: Sven Eckelmann <sven@narfation.org>

net/batman-adv/tp_meter.c
@@ -27,6 +27,7 @@
 #include <linux/etherdevice.h>
 #include <linux/fs.h>
 #include <linux/if_ether.h>
+#include <linux/init.h>
 #include <linux/jiffies.h>
 #include <linux/kernel.h>
 #include <linux/kref.h>