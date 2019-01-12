open-mesh-mirror/batman-adv

commit 8af877b110548408b1fdf7ddbed830be68d7fb05

batman-adv: Remove 'type' argument from access_ok() function
Nobody has actually used the type (VERIFY_READ vs VERIFY_WRITE) argument
of the user address range verification function since we got rid of the
old racy i386-only code to walk page tables by hand.

It existed because the original 80386 would not honor the write protect
bit when in kernel mode, so you had to do COW by hand before doing any
user access.  But we haven't supported that in a long time, and these
days the 'type' argument is a purely historical artifact.

A discussion about extending 'user_access_begin()' to do the range
checking resulted this patch, because there is no way we're going to
move the old VERIFY_xyz interface to that model.  And it's best done at
the end of the merge window when I've done most of my merges, so let's
just get this done once and for all.

Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
[sven@narfation.org: Added compat code]
Signed-off-by: Sven Eckelmann <sven@narfation.org>

compat-include/linux/uaccess.h
@@ -1,0 +1,44 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/* Copyright (C) 2007-2019  B.A.T.M.A.N. contributors:
+ *
+ * Marek Lindner, Simon Wunderlich
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of version 2 of the GNU General Public
+ * License as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ *
+ * This file contains macros for maintaining compatibility with older versions
+ * of the Linux kernel.
+ */
+
+#ifndef _NET_BATMAN_ADV_COMPAT_LINUX_UACCESS_H_
+#define _NET_BATMAN_ADV_COMPAT_LINUX_UACCESS_H_
+
+#include <linux/version.h>
+#include_next <linux/uaccess.h>
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 0, 0)
+
+static inline int batadv_access_ok(int type, const void __user *p,
+				   unsigned long size)
+{
+	return access_ok(type, p, size);
+}
+
+#ifdef access_ok
+#undef access_ok
+#endif
+
+#define access_ok(addr, size)	batadv_access_ok(VERIFY_WRITE, (addr), (size))
+
+#endif /* < KERNEL_VERSION(5, 0, 0) */
+
+#endif	/* _NET_BATMAN_ADV_COMPAT_LINUX_UACCESS_H_ */

net/batman-adv/icmp_socket.c
@@ -147,7 +147,7 @@ static ssize_t batadv_socket_read(struct file *file, char __user *buf,
 	if (!buf || count < sizeof(struct batadv_icmp_packet))
 		return -EINVAL;
 
-	if (!access_ok(VERIFY_WRITE, buf, count))
+	if (!access_ok(buf, count))
 		return -EFAULT;
 
 	error = wait_event_interruptible(socket_client->queue_wait,

net/batman-adv/log.c
@@ -136,7 +136,7 @@ static ssize_t batadv_log_read(struct file *file, char __user *buf,
 	if (count == 0)
 		return 0;
 
-	if (!access_ok(VERIFY_WRITE, buf, count))
+	if (!access_ok(buf, count))
 		return -EFAULT;
 
 	error = wait_event_interruptible(debug_log->queue_wait,

commit 954cd5d73d339baf69206893f797bd54ee100f33

batman-adv: Provide 3-argument access_ok fallback
The kernels 3.16-3.18 use the 3-arguments access_ok macro in header
files. When we remove this version, the build will fail on these kernels.
with:

  include/net/sock.h:1818:22: error: macro "access_ok" passed 3 arguments, but takes just 2
  In file included from net/batman-adv/bridge_loop_avoidance.c:52:0:
  include/net/sock.h: In function ‘skb_do_copy_data_nocache’:
  include/net/sock.h:1818:41: error: macro "access_ok" passed 3 arguments, but takes just 2
     if (!access_ok(VERIFY_READ, from, copy) ||
                                           ^
  include/net/sock.h:1818:8: error: ‘access_ok’ undeclared (first use in this function)
     if (!access_ok(VERIFY_READ, from, copy) ||
          ^~~~~~~~~

Fixes: 8af877b11054 ("batman-adv: Remove 'type' argument from access_ok() function")
Signed-off-by: Sven Eckelmann <sven@narfation.org>

compat-include/linux/uaccess.h
@@ -37,7 +37,12 @@ static inline int batadv_access_ok(int type, const void __user *p,
 #undef access_ok
 #endif
 
-#define access_ok(addr, size)	batadv_access_ok(VERIFY_WRITE, (addr), (size))
+#define access_ok_get(_1, _2, _3 , access_ok_name, ...) access_ok_name
+#define access_ok(...) \
+	access_ok_get(__VA_ARGS__, access_ok3, access_ok2)(__VA_ARGS__)
+
+#define access_ok2(addr, size)	batadv_access_ok(VERIFY_WRITE, (addr), (size))
+#define access_ok3(type, addr, size)	batadv_access_ok((type), (addr), (size))
 
 #endif /* < KERNEL_VERSION(5, 0, 0) */
 