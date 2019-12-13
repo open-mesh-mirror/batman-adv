open-mesh-mirror/batman-adv

commit dcb63377d9de914e15c500aead14f28c4eda8d23

batman-adv: ELP - use wifi tx bitrate as fallback throughput
Some wifi drivers (e.g. ath10k) provide per-station rx/tx values but no
estimated throughput. Setting a better estimate than the default 1 MBit
makes these devices work well with B.A.T.M.A.N. V.

Signed-off-by: René Treffer <treffer@measite.de>
Signed-off-by: Marek Lindner <mareklindner@neomailbox.ch>
Signed-off-by: Sven Eckelmann <sven@narfation.org>

net/batman-adv/bat_v_elp.c
@@ -107,10 +107,15 @@ static u32 batadv_v_elp_get_throughput(struct batadv_hardif_neigh_node *neigh)
 		}
 		if (ret)
 			goto default_throughput;
-		if (!(sinfo.filled & BIT(NL80211_STA_INFO_EXPECTED_THROUGHPUT)))
-			goto default_throughput;
 
-		return sinfo.expected_throughput / 100;
+		if (sinfo.filled & BIT(NL80211_STA_INFO_EXPECTED_THROUGHPUT))
+			return sinfo.expected_throughput / 100;
+
+		/* try to estimate en expected throughput based on reported tx rates */
+		if (sinfo.filled & BIT(NL80211_STA_INFO_TX_BITRATE))
+			return cfg80211_calculate_bitrate(&sinfo.txrate) / 3;
+
+		goto default_throughput;
 	}
 
 	/* if not a wifi interface, check if this device provides data via

commit 7f8f8e5e35a7a17ce3987f7bb31083da0986b2f8

batman-adv: Avoid comment line over 80 characters
Fixes: dcb63377d9de ("batman-adv: ELP - use wifi tx bitrate as fallback throughput")
Signed-off-by: Sven Eckelmann <sven@narfation.org>

net/batman-adv/bat_v_elp.c
@@ -111,7 +111,9 @@ static u32 batadv_v_elp_get_throughput(struct batadv_hardif_neigh_node *neigh)
 		if (sinfo.filled & BIT(NL80211_STA_INFO_EXPECTED_THROUGHPUT))
 			return sinfo.expected_throughput / 100;
 
-		/* try to estimate en expected throughput based on reported tx rates */
+		/* try to estimate the expected throughput based on reported tx
+		 * rates
+		 */
 		if (sinfo.filled & BIT(NL80211_STA_INFO_TX_BITRATE))
 			return cfg80211_calculate_bitrate(&sinfo.txrate) / 3;
 