fe773e9e389a763f38539de3d4782b628676ac99
Sven Eckelmann <sven@narfation.org>
2020-08-17 15:47:25 +0200

    batman-adv: Migrate to linux/prandom.h
    The commit c0842fbc1b18 ("random32: move the pseudo-random 32-bit
    definitions to prandom.h") introduced a new header for the pseudo random
    functions from (previously) linux/random.h. One future goal of the
    prandom.h change is to make code to switch just the new header file and to
    avoid the implicit include. This would allow the removal of the implicit
    include from random.h
    
    Signed-off-by: Sven Eckelmann <sven@narfation.org>

---
+++ compat-include/linux/prandom.h
@@ -0,0 +1,24 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/* Copyright (C) 2007-2020  B.A.T.M.A.N. contributors:
+ *
+ * Marek Lindner, Simon Wunderlich
+ *
+ * This file contains macros for maintaining compatibility with older versions
+ * of the Linux kernel.
+ */
+
+#ifndef _NET_BATMAN_ADV_COMPAT_LINUX_PRANDOM_H_
+#define _NET_BATMAN_ADV_COMPAT_LINUX_PRANDOM_H_
+
+#include <linux/version.h>
+#if LINUX_VERSION_IS_GEQ(5, 8, 1) || \
+    (LINUX_VERSION_IS_GEQ(4, 14, 193) && LINUX_VERSION_IS_LESS(4, 15, 0)) || \
+    (LINUX_VERSION_IS_GEQ(4, 19, 138) && LINUX_VERSION_IS_LESS(4, 20, 0)) || \
+    (LINUX_VERSION_IS_GEQ(5, 4, 57) && LINUX_VERSION_IS_LESS(5, 5, 0)) || \
+    (LINUX_VERSION_IS_GEQ(5, 7, 14) && LINUX_VERSION_IS_LESS(5, 8, 0))
+#include_next <linux/prandom.h>
+#else
+#include <linux/random.h>
+#endif
+
+#endif /* _NET_BATMAN_ADV_COMPAT_LINUX_PRANDOM_H_ */
