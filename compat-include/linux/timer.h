d9bffde8128b2bbc29fe7c49dccaf5965ff4b8cc
Thomas Gleixner <tglx@linutronix.de>
2025-04-05 10:17:26 +0200

    batman-adv: Switch/rename to timer_delete[_sync]()
    timer_delete[_sync]() replaces del_timer[_sync](). Convert the whole tree
    over and remove the historical wrapper inlines.
    
    Conversion was done with coccinelle plus manual fixups where necessary.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    [sven@narfation.org: add compat code]
    Signed-off-by: Sven Eckelmann <sven@narfation.org>

---
+++ compat-include/linux/timer.h
@@ -0,0 +1,23 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/* Copyright (C) B.A.T.M.A.N. contributors:
+ *
+ * Marek Lindner, Simon Wunderlich
+ *
+ * This file contains macros for maintaining compatibility with older versions
+ * of the Linux kernel.
+ */
+
+#ifndef _NET_BATMAN_ADV_COMPAT_LINUX_TIMER_H_
+#define _NET_BATMAN_ADV_COMPAT_LINUX_TIMER_H_
+
+#include <linux/version.h>
+#include_next <linux/timer.h>
+
+#if LINUX_VERSION_IS_LESS(6, 2, 0)
+
+#define timer_delete_sync(_timer) del_timer_sync(_timer)
+#define timer_delete(_timer) del_timer(_timer)
+
+#endif /* LINUX_VERSION_IS_LESS(6, 2, 0) */
+
+#endif /* _NET_BATMAN_ADV_COMPAT_LINUX_TIMER_H_ */
