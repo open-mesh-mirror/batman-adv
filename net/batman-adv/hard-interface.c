open-mesh-mirror/batman-adv

commit 035409656fc7829303bdd368564a37bdcf4398b0

batman-adv: adopt netdev_hold() / netdev_put()
Add a device tracker to struct batadv_hard_iface to help
debugging of network device refcount imbalances.

Signed-off-by: Eric Dumazet <edumazet@google.com>
[sven@narfation.org: fix kernel-doc, adopt for softif reference]
Signed-off-by: Sven Eckelmann <sven@narfation.org>

net/batman-adv/hard-interface.c
@@ -51,7 +51,7 @@ void batadv_hardif_release(struct kref *ref)
 	struct batadv_hard_iface *hard_iface;
 
 	hard_iface = container_of(ref, struct batadv_hard_iface, refcount);
-	dev_put(hard_iface->net_dev);
+	netdev_put(hard_iface->net_dev, &hard_iface->dev_tracker);
 
 	kfree_rcu(hard_iface, rcu);
 }
@@ -726,6 +726,7 @@ int batadv_hardif_enable_interface(struct batadv_hard_iface *hard_iface,
 	kref_get(&hard_iface->refcount);
 
 	dev_hold(soft_iface);
+	netdev_hold(soft_iface, &hard_iface->softif_dev_tracker, GFP_ATOMIC);
 	hard_iface->soft_iface = soft_iface;
 	bat_priv = netdev_priv(hard_iface->soft_iface);
 
@@ -782,7 +783,7 @@ err_upper:
 	netdev_upper_dev_unlink(hard_iface->net_dev, soft_iface);
 err_dev:
 	hard_iface->soft_iface = NULL;
-	dev_put(soft_iface);
+	netdev_put(soft_iface, &hard_iface->softif_dev_tracker);
 	batadv_hardif_put(hard_iface);
 	return ret;
 }
@@ -849,7 +850,7 @@ void batadv_hardif_disable_interface(struct batadv_hard_iface *hard_iface)
 	/* delete all references to this hard_iface */
 	batadv_purge_orig_ref(bat_priv);
 	batadv_purge_outstanding_packets(bat_priv, hard_iface);
-	dev_put(hard_iface->soft_iface);
+	netdev_put(hard_iface->soft_iface, &hard_iface->softif_dev_tracker);
 
 	netdev_upper_dev_unlink(hard_iface->net_dev, hard_iface->soft_iface);
 	batadv_hardif_recalc_extra_skbroom(hard_iface->soft_iface);
@@ -873,15 +874,15 @@ batadv_hardif_add_interface(struct net_device *net_dev)
 	ASSERT_RTNL();
 
 	if (!batadv_is_valid_iface(net_dev))
-		goto out;
-
-	dev_hold(net_dev);
+		return NULL;
 
 	hard_iface = kzalloc(sizeof(*hard_iface), GFP_ATOMIC);
 	if (!hard_iface)
-		goto release_dev;
+		return NULL;
 
+	netdev_hold(net_dev, &hard_iface->dev_tracker, GFP_ATOMIC);
 	hard_iface->net_dev = net_dev;
+
 	hard_iface->soft_iface = NULL;
 	hard_iface->if_status = BATADV_IF_NOT_IN_USE;
 
@@ -907,11 +908,6 @@ batadv_hardif_add_interface(struct net_device *net_dev)
 	batadv_hardif_generation++;
 
 	return hard_iface;
-
-release_dev:
-	dev_put(net_dev);
-out:
-	return NULL;
 }
 
 static void batadv_hardif_remove_interface(struct batadv_hard_iface *hard_iface)

compat-include/linux/netdevice.h
@@ -35,6 +35,26 @@ static inline void batadv_dev_hold(struct net_device *dev)
 
 #endif /* LINUX_VERSION_IS_LESS(5, 15, 0) */
 
+#if LINUX_VERSION_IS_LESS(5, 17, 0)
+
+typedef struct {} netdevice_tracker;
+
+#define netdev_hold(__dev, __tracker, __gfp) \
+	dev_hold(__dev)
+
+#define netdev_put(__dev, __tracker) \
+	dev_put(__dev)
+
+#elif LINUX_VERSION_IS_LESS(6, 0, 0)
+
+#define netdev_hold(__dev, __tracker, __gfp) \
+	dev_hold_track(__dev, __tracker, __gfp)
+
+#define netdev_put(__dev, __tracker) \
+	dev_put_track(__dev, __tracker)
+
+#endif /* LINUX_VERSION_IS_LESS(5, 17, 0) */
+
 #if LINUX_VERSION_IS_LESS(5, 18, 0)
 
 static inline int batadv_netif_rx(struct sk_buff *skb)

net/batman-adv/types.h
@@ -186,6 +186,9 @@ struct batadv_hard_iface {
 	/** @net_dev: pointer to the net_device */
 	struct net_device *net_dev;
 
+	/** @dev_tracker: device tracker for @net_dev */
+	netdevice_tracker dev_tracker;
+
 	/** @refcount: number of contexts the object is used */
 	struct kref refcount;
 
@@ -200,6 +203,9 @@ struct batadv_hard_iface {
 	 *  interface
 	 */
 	struct net_device *soft_iface;
+
+	/** @softif_dev_tracker: device tracker for @soft_iface */
+	netdevice_tracker softif_dev_tracker;
 
 	/** @rcu: struct used for freeing in an RCU-safe manner */
 	struct rcu_head rcu;

commit 472d63bf39f835f5b46d154aa1c6261f3ff151c0

batman-adv: Fix double-hold of meshif when getting enabled
Fixes: 035409656fc7 ("batman-adv: adopt netdev_hold() / netdev_put()")
Signed-off-by: Sven Eckelmann <sven@narfation.org>

net/batman-adv/hard-interface.c
@@ -725,7 +725,6 @@ int batadv_hardif_enable_interface(struct batadv_hard_iface *hard_iface,
 
 	kref_get(&hard_iface->refcount);
 
-	dev_hold(mesh_iface);
 	netdev_hold(mesh_iface, &hard_iface->meshif_dev_tracker, GFP_ATOMIC);
 	hard_iface->mesh_iface = mesh_iface;
 	bat_priv = netdev_priv(hard_iface->mesh_iface);