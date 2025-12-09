01e695cec2aeb7303d348c66e6e781736ad4b9ff
Sven Eckelmann <sven@narfation.org>
2025-12-09 14:44:34 +0100

    batman-adv: Fix build against newest stabe kernels
    The newest stable kernels in commit 3cd39bc3b11b ("kernel.h: Move
    ARRAY_SIZE() to a separate header"). For these kernel versions, it is now
    necessary to use the new linux/array_size.h header.
    
    Signed-off-by: Sven Eckelmann <sven@narfation.org>

--- compat-include/linux/array_size.h
+++ compat-include/linux/array_size.h
@@ -11,7 +11,10 @@
 #define _NET_BATMAN_ADV_COMPAT_LINUX_ARRAY_SIZE_H_
 
 #include <linux/version.h>
-#if LINUX_VERSION_IS_GEQ(6, 7, 0)
+#if (LINUX_VERSION_IS_GEQ(5, 15, 197) && LINUX_VERSION_IS_LESS(5, 16, 0)) || \
+    (LINUX_VERSION_IS_GEQ(6, 1, 159) && LINUX_VERSION_IS_LESS(6, 2, 0)) || \
+    (LINUX_VERSION_IS_GEQ(6, 6, 118) && LINUX_VERSION_IS_LESS(6, 7, 0)) || \
+    LINUX_VERSION_IS_GEQ(6, 7, 0)
 #include_next <linux/array_size.h>
 #else
 #include <linux/kernel.h>
