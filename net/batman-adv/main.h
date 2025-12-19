35208d1234584c536db431441445ce82f09273f6
Simon Wunderlich <sw@simonwunderlich.de>
2025-12-19 08:11:22 +0100

    batman-adv: Start new development cycle
    Signed-off-by: Simon Wunderlich <sw@simonwunderlich.de>

--- net/batman-adv/main.h
+++ net/batman-adv/main.h
@@ -13,7 +13,7 @@
 #define BATADV_DRIVER_DEVICE "batman-adv"
 
 #ifndef BATADV_SOURCE_VERSION
-#define BATADV_SOURCE_VERSION "2025.5"
+#define BATADV_SOURCE_VERSION "2026.0"
 #endif
 
 /* B.A.T.M.A.N. parameters */
