65b5ac9b9728687dfcc9f558644370ab6e1771ba
Sven Eckelmann <sven@narfation.org>
2026-05-02 13:20:40 +0200

    batman-adv: Disable time compat for kernels with native support
    Some stable kernels got the new timer_* API backported. These kernels
    should not use the compat layer to avoid undefined behaviors when mixing
    APIs.
    
    Signed-off-by: Sven Eckelmann <sven@narfation.org>

--- compat-include/linux/timer.h
+++ compat-include/linux/timer.h
@@ -13,7 +13,9 @@
 #include <linux/version.h>
 #include_next <linux/timer.h>
 
-#if LINUX_VERSION_IS_LESS(6, 2, 0)
+#if LINUX_VERSION_IS_LESS(6, 2, 0) && \
+    !(LINUX_VERSION_IS_GEQ(5, 15, 200) && LINUX_VERSION_IS_LESS(5, 16, 0)) && \
+    !(LINUX_VERSION_IS_GEQ(6, 1, 158) && LINUX_VERSION_IS_LESS(6, 2, 0))
 
 #define timer_delete_sync(_timer) del_timer_sync(_timer)
 #define timer_delete(_timer) del_timer(_timer)
